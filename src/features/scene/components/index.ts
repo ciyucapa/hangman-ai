561019b2e49ee7de584c2ee3b85848fb24b63787
Cindy Yurley Cáceres Parada <55037302+ciyucapa@users.noreply.github.com>
2026-06-29 18:47:20 -0500

    refactor(scene): reorganize scene components architecture (#2)

--- src/features/scene/components/index.ts
+++ src/features/scene/components/index.ts
@@ -1,1 +1,5 @@
 export { default as Scene } from './Scene';
+export { default as Environment } from './Environment';
+export { default as Ground } from './Ground';
+export { default as Lights } from './Light';
+export { default as PlaceholderCharacter } from './PlaceholderCharacter';
